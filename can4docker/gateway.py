0b06923e97aed63790550aeed09c9798a6f87d4a
Wiktor S. Ovalle Correa <w.s.ovalle@esworkshop.pl>
2021-12-22 12:08:31 +0100

    Enable CAN FD routing

--- can4docker/gateway.py
+++ can4docker/gateway.py
@@ -22,11 +22,11 @@ class Gateway(object):
         if (not self.rules.get(src_netdev, None)):
             self.rules[src_netdev] = []
         if not dst_netdev in self.rules[src_netdev]:
-            utils.sh("cangw -A -s {} -d {} -e".format(src_netdev, dst_netdev))
+            utils.sh("cangw -A -s {} -d {} -eX".format(src_netdev, dst_netdev))
             self.rules[src_netdev].append(dst_netdev)
 
     def remove_rule(self, src_netdev, dst_netdev):
         rules = self.rules.get(src_netdev, None)
         if rules and dst_netdev in rules:
-            utils.sh("cangw -D -s {} -d {} -e".format(src_netdev, dst_netdev))
+            utils.sh("cangw -D -s {} -d {} -eX".format(src_netdev, dst_netdev))
             self.rules.remove(dst_netdev)
