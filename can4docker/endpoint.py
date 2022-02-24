00f341132d6e5deb5e6242b91ce625a6965f9650
Jonathan Haws <hawsjr@gmail.com>
2022-02-24 22:02:38 +0000

    Fix typos in logs

--- can4docker/endpoint.py
+++ can4docker/endpoint.py
@@ -18,17 +18,17 @@ class EndPoint(object):
         self.peer_if_name = "{}p".format(self.if_name)
 
     def create_resource(self):
-        LOGGER.debug("Creating resource for enpoint {}...".format(self.endpoint_id))
+        LOGGER.debug("Creating resource for endpoint {}...".format(self.endpoint_id))
         with pyroute2.IPDB() as ipdb:
             with ipdb.create(kind='vxcan', ifname=self.if_name,
                              peer=self.peer_if_name) as link:
                 link.up()
-        LOGGER.debug("Created resource for enpoint {}.".format(self.endpoint_id))
+        LOGGER.debug("Created resource for endpoint {}.".format(self.endpoint_id))
 
     def delete_resource(self):
-        LOGGER.debug("Deleting resource for enpoint {}...".format(self.endpoint_id))
+        LOGGER.debug("Deleting resource for endpoint {}...".format(self.endpoint_id))
         with pyroute2.IPDB() as ipdb:
             with ipdb.interfaces[self.if_name] as link:
                 link.down()
                 link.remove()
-        LOGGER.debug("Deleted resource for enpoint {}.".format(self.endpoint_id))
+        LOGGER.debug("Deleted resource for endpoint {}.".format(self.endpoint_id))
